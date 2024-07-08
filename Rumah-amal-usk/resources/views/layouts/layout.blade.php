ce341d563588c854b29f206bb5d1f562c604e12e
ridhoF-1 <ridho321rg@gmail.com>
2024-07-08 11:38:02 +0700

    Update layout.blade.php

--- Rumah-amal-usk/resources/views/layouts/layout.blade.php
+++ Rumah-amal-usk/resources/views/layouts/layout.blade.php
@@ -100,7 +100,7 @@
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.062950078828!2d95.3687264!3d5.5709969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304037d79398cc65%3A0x164fb653d9c4a1f7!2sRumah%20Amal%20Masjid%20Jamik%20USK!5e0!3m2!1sen!2sid!4v1688584573276!5m2!1sen!2sid"
                     width="300px"
                     height="300px"
-                    style="border:0;"
+                    style="border:0; border-radius: 15px;"
                     allowfullscreen=""
                     loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"
