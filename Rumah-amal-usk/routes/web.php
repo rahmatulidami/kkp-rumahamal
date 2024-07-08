eae179e2a80348697e268ade53add50f8b6ee9d2
Rahmatul idami <rahmatul_i@mhs.unsyiah.ac.id>
2024-07-08 15:35:14 +0700

    menambahkan detail-berita

--- Rumah-amal-usk/routes/web.php
+++ Rumah-amal-usk/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landing/home');
 });
+
+Route::get('/berita', function () {
+    return view('landing/berita');
+});
+
+Route::get('/detail-berita', function () {
+    return view('landing/detail-berita');
+});
