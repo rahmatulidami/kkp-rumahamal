afe7ef2be03584480aa92ca5a26accf843883f75
Rahmatul idami <rahmatul_i@mhs.unsyiah.ac.id>
2024-07-09 11:22:35 +0700

    tambah youtube

--- Rumah-amal-usk/resources/views/landing/home.blade.php
+++ Rumah-amal-usk/resources/views/landing/home.blade.php
@@ -366,18 +366,18 @@
 <section id="call-to-action" class="call-to-action section dark-background">
 
 <div class="container">
-  <img src="assets/img/cta-bg.jpg" alt="">
   <div class="content row justify-content-center" data-aos="zoom-in" data-aos-delay="100">
     <div class="col-xl-10">
       <div class="text-center">
-        <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox play-btn"></a>
-        <h3>Call To Action</h3>
-        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <a class="cta-btn" href="#">Call To Action</a>
+        <div class="video-container">
+          <iframe id="youtube-video" src="https://www.youtube.com/embed/C1Asqu3uHxs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
       </div>
     </div>
   </div>
 </div>
+
+
 
 </section><!-- /Call To Action Section -->
 
