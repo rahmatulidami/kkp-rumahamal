b3efcbbd7b907ce18ee2d31a51c85b574915b276
Rahmatul idami <rahmatul_i@mhs.unsyiah.ac.id>
2024-07-18 14:49:41 +0700

    update profil rumah amal

--- Rumah-amal-usk/resources/views/profil/profil.blade.php
+++ Rumah-amal-usk/resources/views/profil/profil.blade.php
@@ -4,41 +4,43 @@
 
 @section('content')
 
-  <main class="main">
+<main class="main">
 
-    <!-- Page Title -->
-    <div class="page-title">
-      <div class="heading">
-        <div class="container">
-          <div class="row d-flex justify-content-center text-center">
-            <div class="col-lg-8">
-              <h1>PROFIL</h1>
-            </div>
+  <!-- Page Title -->
+  <div class="page-title">
+    <div class="heading">
+      <div class="container">
+        <div class="row d-flex justify-content-center text-center">
+          <div class="col-lg-8">
+            <h1>PROFIL</h1>
           </div>
         </div>
       </div>
-      <nav class="breadcrumbs">
-        <div class="container">
+    </div>
+
+    <nav class="breadcrumbs">
+      <div class="container">
           <ol>
             <li><a href="/">Home</a></li>
             <li class="current">Profil</li>
           </ol>
-        </div>
-      </nav>
-    </div><!-- End Page Title -->
+      </div>
+    </nav>
+  </div>
+  <!-- End Page Title -->
 
-    <!-- Blog Posts Section -->
     <!-- About Section -->
-    <section id="about" class="about section">
+  <section id="about" class="about section">
 
-      <div class="container">
+    <div class="container">
 
-        <div class="row gy-4">
-          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-            <img src="assets/img/profil/usk.png" class="img-fluid rounded-4 mb-4" alt="">
-            </div>
-          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="250">
-            <div class="content ps-0 ps-lg-5">
+      <div class="row gy-4">
+        <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+          <img src="assets/img/profil/usk.png" class="img-fluid rounded-4 mb-4" alt="">
+        </div>
+          
+        <div class="col-lg-6" data-aos="fade-up" data-aos-delay="250">
+          <div class="content ps-0 ps-lg-5">
               <h3>RUMAH AMAL MASJID JAMIK USK</h2>
               <p>
               Rumah Amal Masjid Jamik Universitas Syiah Kuala adalah lembaga amil zakat, infaq dan shadaqah yang telah berdiri sejak 2016 untuk menghimpun dan menyalurkan dana (Zakat, Infaq dan Shadaqah).
@@ -49,30 +51,31 @@
               <br>
               Rumah Amal Masjid Jamik USK pada tahun 2023 telah menjalin kerjasama dengan lembaga Amil Zakat Nasional (LAZNAS) Rumah Amal Salman dengan Keputusan Mentri Agama Republik Indonesia No 854 Tahun 2023, sehingga Rumah Amal Masjid Jamik USK secara resmi menjadi bagian dari LAZNAS Rumah Amal Salman dengan Surat Keputusan Pengurus Rumah Amal Nomor 68 Tahun 2023.
               </p>
-            </div>
           </div>
         </div>
-
       </div>
 
-    </section><!-- /About Section -->
+    </div>
 
-    <section id="visi" class="visi section">
+  </section><!-- /About Section -->
 
-    <!-- Section Title -->
-    <div class="container section-title" data-aos="fade-up">
-        <h2>VISI & MISI RUMAH AMAL USK</h2>
-    </div><!-- End Section Title -->
+  <section id="visi" class="visi section">
+
+  <!-- Section Title -->
+  <div class="container section-title" data-aos="fade-up">
+    <h2>VISI & MISI RUMAH AMAL USK</h2>
+  </div><!-- End Section Title -->
 
         <div class="icon-boxes position-relative" data-aos="fade-up" data-aos-delay="200">
             <div class="container position-relative">
                 <div class="row gy-4 mt-5">
                     <div class="col-xl-4 col-md-6">
-                    <div class="visi-box">
+                      <div class="visi-box">
                         <h4 class="title">VISI</h4>
                         <p>Menjadi Lembaga Amil Zakat dan pemberdayaan ekonomi umat yang inovatif, responsif, profesional dan terkemuka untuk kemaslahatan bersama yang berbasis masjid.</p>
-                        </div>
+                      </div>
                     </div>
+
                     <div class="col-xl-4 col-md-6">
                         <div class="visi-box">
                             <h4 class="title">MISI</h4>
@@ -133,8 +136,21 @@
             </div>
         </div>
 
-    </section><!-- /About Section -->
+  </section><!-- /visi-mmisi Section -->
+
+  <!-- Section struktur -->
+   <section class="struktur-organisasi" id="struktur-organisasi section">
+    <!-- Section Title -->
+    <div class="container section-title" data-aos="fade-up">
+      <h2>STRUKTUR ORGANISASI RUMAH AMAL USK</h2>
+    </div><!-- End Section Title -->
+
+    <div class="gambar">
+      <img src="assets/img/struktur.png" alt="">
+    </div>
+
+   </section>
 
 
-  </main>
+</main>
 @endsection
