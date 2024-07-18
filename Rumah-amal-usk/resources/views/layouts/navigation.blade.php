165e4fc775d2dfd2a27ff805e1d264e6981f83b9
Rahmatul idami <rahmatul_i@mhs.unsyiah.ac.id>
2024-07-18 12:16:55 +0700

    update dashboard admin

--- Rumah-amal-usk/resources/views/layouts/navigation.blade.php
+++ Rumah-amal-usk/resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('home') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img src="/assets/img//logorumah.png" alt="Logo" class="block h-9 w-auto" />
                     </a>
                 </div>
 
@@ -17,6 +17,9 @@
                     </x-nav-link>
                 </div>
             </div>
+
+
+            
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
