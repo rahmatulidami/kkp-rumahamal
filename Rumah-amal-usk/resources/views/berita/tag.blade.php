1eb78f49034c16f50cfb8b384dbd523fd9b9c369
rahmatulidami <rahmatul_i@mhs.usk.ac.id>
2025-04-30 14:25:58 +0700

    pagination di tag

--- Rumah-amal-usk/resources/views/berita/tag.blade.php
+++ Rumah-amal-usk/resources/views/berita/tag.blade.php
@@ -60,26 +60,62 @@
         </div>
     </section><!-- /Blog Posts Section -->
 
-    <!-- Blog Pagination Section -->
-    <section id="blog-pagination" class="blog-pagination section">
-        <div class="container">
-            <div class="d-flex justify-content-center">
-                <ul>
-                    @if($pagination['current_page'] > 1)
-                        <li><a href="{{ url('berita?page=' . ($pagination['current_page'] - 1) . '&tag=' . request('tag')) }}"><i class="bi bi-chevron-left"></i></a></li>
-                    @endif
+<!-- Pagination -->
+<section id="gallery-pagination" class="gallery-pagination section">
+    <div class="container">
+        <div class="d-flex justify-content-center">
+            <ul class="pagination-list">
+                @if($pagination['current_page'] > 1)
+                    <li class="page-item">
+                        <a href="{{ url("tag/$tag?page=" . ($pagination['current_page'] - 1)) }}" class="page-link prev" aria-label="Previous">
+                            <i class="bi bi-chevron-left"></i>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                    </li>
+                @endif
 
-                    @for($i = 1; $i <= $pagination['total_pages']; $i++)
-                        <li><a href="{{ url('berita?page=' . $i . '&tag=' . request('tag')) }}" class="{{ $pagination['current_page'] == $i ? 'active' : '' }}">{{ $i }}</a></li>
-                    @endfor
-
-                    @if($pagination['current_page'] < $pagination['total_pages'])
-                        <li><a href="{{ url('berita?page=' . ($pagination['current_page'] + 1) . '&tag=' . request('tag')) }}"><i class="bi bi-chevron-right"></i></a></li>
+                {{-- First page --}}
+                @if($pagination['current_page'] > 3)
+                    <li class="page-item">
+                        <a href="{{ url("tag/$tag?page=1") }}" class="page-link">1</a>
+                    </li>
+                    @if($pagination['current_page'] > 4)
+                        <li class="page-item disabled dots">
+                            <span class="page-link">...</span>
+                        </li>
                     @endif
-                </ul>
-            </div>
+                @endif
+
+                {{-- Middle pages --}}
+                @for($i = max(1, $pagination['current_page'] - 2); $i <= min($pagination['total_pages'], $pagination['current_page'] + 2); $i++)
+                    <li class="page-item {{ $pagination['current_page'] == $i ? 'active' : '' }}">
+                        <a href="{{ url("tag/$tag?page=$i") }}" class="page-link">{{ $i }}</a>
+                    </li>
+                @endfor
+
+                {{-- Last page --}}
+                @if($pagination['current_page'] < $pagination['total_pages'] - 2)
+                    @if($pagination['current_page'] < $pagination['total_pages'] - 3)
+                        <li class="page-item disabled dots">
+                            <span class="page-link">...</span>
+                        </li>
+                    @endif
+                    <li class="page-item">
+                        <a href="{{ url("tag/$tag?page=" . $pagination['total_pages']) }}" class="page-link">{{ $pagination['total_pages'] }}</a>
+                    </li>
+                @endif
+
+                @if($pagination['current_page'] < $pagination['total_pages'])
+                    <li class="page-item">
+                        <a href="{{ url("tag/$tag?page=" . ($pagination['current_page'] + 1)) }}" class="page-link next" aria-label="Next">
+                            <i class="bi bi-chevron-right"></i>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </li>
+                @endif
+            </ul>
         </div>
-    </section><!-- /Blog Pagination Section -->
-
+    </div>
+</section>
 </main>
 @endsection
